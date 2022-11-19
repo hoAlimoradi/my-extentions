ef6cf7331e92ccd41b9d220917db4ed5b73a2f42
hosein alimoradi <ho.alimoradi@secnerd.ir>
2022-11-19 09:49:10 +0330

    Initial Commit

---
+++ my-extentions/my_extentions.h
@@ -0,0 +1,18 @@
+//
+//  my_extentions.h
+//  my-extentions
+//
+//  Created by Hosein Alimoradi on 8/28/1401 AP.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for my_extentions.
+FOUNDATION_EXPORT double my_extentionsVersionNumber;
+
+//! Project version string for my_extentions.
+FOUNDATION_EXPORT const unsigned char my_extentionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <my_extentions/PublicHeader.h>
+
+
